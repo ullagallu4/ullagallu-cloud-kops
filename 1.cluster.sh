8f097978b48ea65d5ab66e1c9a883d2778448fcc
kops <kops@gmail.com>
2023-12-21 21:12:18 +0530

    match

--- 1.cluster.sh
+++ 1.cluster.sh
@@ -3,13 +3,13 @@ kops create cluster \
     --cloud=aws \
     --name=ullagallu.cloud \
     --node-count=2 \
-    --node-size=t3a.small \
+    --node-size=t3a.medium \
     --node-volume-size=8 \
     --control-plane-count=1 \
-    --control-plane-size=t3a.small \
+    --control-plane-size=t3a.medium \
     --control-plane-volume-size=8 \
     --zones=ap-south-1a \
     --state=s3://ullagallu.kops.cloud \
     --dns=public \
     --dns-zone=ullagallu.cloud \
-    --networking=cilium
+    --networking=calico
--- 4.validate.sh
+++ 4.validate.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-time kops validate cluster --wait 10m
+kops validate cluster --wait 10m
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 kubectl apply -f sc.yaml
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}'
+kubectl apply -f jenkins.yaml
+helm install sonarqube sonarqube/ -n devops-tools
+
