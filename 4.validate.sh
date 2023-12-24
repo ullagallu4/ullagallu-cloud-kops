1a236e5c5b0e66ff9c440ceb2f62838c6fb60c6c
kops <kops@gmail.com>
2023-12-24 10:32:18 +0530

    removed sonarqube

--- 4.validate.sh
+++ 4.validate.sh
@@ -2,9 +2,9 @@
 kops validate cluster --wait 10m
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 kubectl apply -f sc.yaml
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}'
-kubectl apply -f jenkins.yaml
-helm install sonarqube sonarqube/ -n devops-tools
+#kubectl create namespace argocd
+#kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+#kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}'
+#kubectl apply -f jenkins.yaml
+#helm install sonarqube sonarqube/ -n devops-tools
 
